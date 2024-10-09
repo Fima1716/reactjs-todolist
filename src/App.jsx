ba6a35816ac6117acebcab77feadc0263ad6a73a
Тимофеев Ефим <efimt74@gmail.com>
2024-10-10 01:54:53 +0300

    start of development

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import TodoInput from "./components/TodoInput/TodoInput";
+import TodoList from "./components/TodoList/TodoList";
+function App() {
+  return (
+    <main>
+      <TodoInput />
+      <TodoList />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/TodoInput/TodoInput.jsx
@@ -0,0 +1,10 @@
+import "../TodoInput/TodoInput.css";
+
+export default function TodoInput() {
+  return (
+    <header>
+      <input type="text" placeholder="Enter todo..." />
+      <button>Add</button>
+    </header>
+  );
+}
